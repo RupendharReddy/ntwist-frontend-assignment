cf2dfc26b2a3e13527a1ba846fadfbb51420dfb9
VARUGU RUPENDHAR REDDY <varugurupendharreddy@gmail.com>
2025-05-15 14:07:53 +0530

    UI and functionality Developed for Sorting page

---
+++ assest/js/sort.js
@@ -0,0 +1,18 @@
+function sortList() {
+  const ul = document.getElementById("nameList");
+  const items = Array.from(ul.getElementsByTagName("li"));
+  const sorted = items.sort((a, b) =>
+    a.textContent.localeCompare(b.textContent)
+  );
+  ul.innerHTML = "";
+  sorted.forEach((item) => ul.appendChild(item));
+}
+function sortListReverse() {
+  const ul = document.getElementById("nameListReverse");
+  const items = Array.from(ul.getElementsByTagName("li"));
+  const sorted = items.sort((a, b) =>
+    b.textContent.localeCompare(a.textContent)
+  );
+  ul.innerHTML = "";
+  sorted.forEach((item) => ul.appendChild(item));
+}
